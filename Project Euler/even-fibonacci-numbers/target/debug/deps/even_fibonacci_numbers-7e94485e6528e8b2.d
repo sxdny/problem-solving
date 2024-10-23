ad824bf73c8c6c204e68d427020ee6aee1dc4083
sxdny <67187745+sxdny@users.noreply.github.com>
2024-10-23 19:43:09 +0200

    delete even../target from remote

--- Project Euler/even-fibonacci-numbers/target/debug/deps/even_fibonacci_numbers-7e94485e6528e8b2.d
+++
@@ -1,5 +0,0 @@
-/Users/sidnezsbo/Documents/GitHub/problem-solving/Project Euler/even-fibonacci-numbers/target/debug/deps/even_fibonacci_numbers-7e94485e6528e8b2: src/main.rs
-
-/Users/sidnezsbo/Documents/GitHub/problem-solving/Project Euler/even-fibonacci-numbers/target/debug/deps/even_fibonacci_numbers-7e94485e6528e8b2.d: src/main.rs
-
-src/main.rs:
--- Project Euler/even-fibonacci-numbers/target/debug/even-fibonacci-numbers.d
+++
@@ -1,1 +0,0 @@
-/Users/sidnezsbo/Documents/GitHub/problem-solving/Project\ Euler/even-fibonacci-numbers/target/debug/even-fibonacci-numbers: /Users/sidnezsbo/Documents/GitHub/problem-solving/Project\ Euler/even-fibonacci-numbers/src/main.rs
